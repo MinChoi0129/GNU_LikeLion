bf7ccca906b084567c261ae8467e45f84d893522
정시은 <jeongsieun@jeongsieun-ui-MacBookPro.local>
2023-11-14 21:05:06 +0900

    sieun jackupham

--- GNU_LikeLion/views_page.py
+++ GNU_LikeLion/views_page.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main(request):
     return render(request, "main.html")
+
+def planpage(request):
+    return render(request, "planpage.html")
