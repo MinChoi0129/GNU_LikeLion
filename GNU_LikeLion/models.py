7bf4514f8898da95664ddcec2d17a80959e32300
psuno01 <p20035357@gmail.com>
2023-11-14 18:50:56 +0900

    1114_1

--- GNU_LikeLion/models.py
+++ GNU_LikeLion/models.py
@@ -3,12 +3,11 @@ from django.db import models
 # Create your models here.
 
 
-# activity records 게시글
-from django.db import models
-
+# activity record 게시글
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
