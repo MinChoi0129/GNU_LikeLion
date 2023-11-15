d0f278229638f572b35dcaaced930f2230cf7345
shinjiaaa <134384192+shinjiaaa@users.noreply.github.com>
2023-11-15 15:22:19 +0900

    Update views_page.py

--- GNU_LikeLion/views_page.py
+++ GNU_LikeLion/views_page.py
@@ -6,10 +6,10 @@ from .models import Post
 def main(request):
     return render(request, "main.html")
 
-<<<<<<< HEAD
+
 def introduce(request):
     return render(request, "introduce.html")
-=======
+
 
 def planPage(request):
     return render(request, "planPage.html")
@@ -34,8 +34,7 @@ def activity(request):
 # activity record 게시글
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'activity.html', {'posts': posts})
->>>>>>> e4f3dc1fb43890591b330458479b78ae1d9320f8
+    return render(request, "activity.html", {"posts": posts})
 
 
 def projectDetail(request):
