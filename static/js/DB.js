eb3ed01af7d6ca17137142b763572987dceffed3
sidonge <wjdtldms1817@naver.com>
2023-11-19 21:18:21 +0900

    dd

--- static/js/DB.js
+++ static/js/DB.js
@@ -6,7 +6,10 @@ export class DataBase{
 
         // 월 바뀔때마다 적용되는 css 추가해야됨 
         this.monDeg = {
-            deg : [0, 30, 60, 90, 0, 0, 0, 0, 0, -90, -60, -30]
+            deg : [0, 30, 60, 90, 0, 0, 0, 0, 0, -90, -60, -30],
+            moveX : [-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10],
+            moveY : [-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-25],
+            fontSize : [50,0,0,0,0,0,0,0,0,0,0,0]
         };
         this.transitionSpeed = [300, 150, 100];
     }
@@ -14,13 +17,10 @@ export class DataBase{
     changeSection(){
         for(let i=0; i<this.MonSection.length; i++){
             
-            this.MonSection[i].style.transition = 'all 0.3s linear 0s';
-            this.MonSection[i].style.transform = `rotate(${this.monDeg.deg[i]}deg)`;
-        }
-        // let deg;
-        // for(let i=0; i<this.MonSection.length; i++){
-        //     deg = this.monDeg.deg[i];        
-        // }
+            this.MonSection[i].style.transition = 'all 0.3s linear 0s'; //deg각도대로 움직일 때의 속도
+            this.MonSection[i].style.transform = `rotate(${this.monDeg.deg[i]}deg) translate(${this.monDeg.moveX[i]}rem, ${this.monDeg.moveY[i]}rem)`;
+            
+            this.MonSection[i].style.fontSize = `${this.monDeg.fontSize[i]}px`;
+        }        
     }
 }
-
