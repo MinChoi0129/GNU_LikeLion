e32eb3cc5cb3ebea6f6761fc8c3fbed329d13699
sidonge <wjdtldms1817@naver.com>
2023-11-20 18:28:44 +0900

    ddd

--- static/js/DB.js
+++ static/js/DB.js
@@ -1,14 +1,14 @@
 export class DataBase{
     constructor(){
         this.Num = 0;
-        this.MaxNum = 11;
+        this.MaxNum = 20;
         this.MonSection = document.querySelectorAll(".Mon");
 
         // 월 바뀔때마다 적용되는 css 추가해야됨 
         this.monDeg = {
             deg : [0, 30, 60, 90, 0, 0, 0, 0, 0, -90, -60, -30],
-            moveX : [-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10],
-            moveY : [-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-25],
+            moveX: [-12, -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10], // X 축 이동
+            moveY: [-35, -32, -28, -23, -18, -14, -10, -7, -5, -3, -2, -1],
             fontSize : [50,0,0,0,0,0,0,0,0,0,0,0]
         };
         this.transitionSpeed = [300, 150, 100];
@@ -23,4 +23,7 @@ export class DataBase{
             this.MonSection[i].style.fontSize = `${this.monDeg.fontSize[i]}px`;
         }        
     }
+
+    
 }
+    
--- static/js/scrollEvent.js
+++ static/js/scrollEvent.js
@@ -53,3 +53,44 @@ export class ScrollEvent{
         }
     }
 }
+
+// import { database } from './planpage.js';
+
+// export class ScrollEvent {
+//     constructor() {
+//         this.MonSection = database.MonSection;
+//         window.addEventListener('wheel', this.wheelEvent.bind(this));
+//         this.canwheel = true;
+//     }
+
+//     wheelEvent(data) {
+//         if (this.canwheel) {
+//             this.canwheel = false;
+//             const wheelValue = data.wheelDelta;
+
+//             if (wheelValue < 0) {
+//                 database.Num++;
+//                 if (database.Num >= database.MaxNum) {
+//                     database.Num = 0;
+//                 }
+//             } else if (wheelValue > 0) {
+//                 database.Num--;
+//                 if (database.Num < 0) {
+//                     database.Num = database.MaxNum - 1;
+//                 }
+//             }
+
+//             const MonSectionTemp = [...database.MonSection];
+//             const temp = MonSectionTemp.pop();
+//             MonSectionTemp.unshift(temp);
+//             database.MonSection = MonSectionTemp.slice();
+
+//             database.changeSection();
+//             console.log(`Num : ${database.Num}`);
+
+//             setTimeout(() => {
+//                 this.canwheel = true;
+//             }, 200);
+//         }
+//     }
+// }
