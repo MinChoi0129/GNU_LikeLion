70543e56d367727e8404942735f9d25f0754b38c
kimminji000 <minji6764@naver.com>
2023-11-20 18:00:39 +0900

    회전

--- static/js/DB.js
+++ static/js/DB.js
@@ -6,9 +6,11 @@ export class DataBase{
 
         // 월 바뀔때마다 적용되는 css 추가해야됨 
         this.monDeg = {
-            deg : [0, 30, 60, 90, 0, 0, 0, 0, 0, -90, -60, -30],
+            deg : [0, 20, 30, 40, 0, 0, 0, 0, 0, -40, -30, -20],
             moveX : [-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10,-10],
             moveY : [-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-35,-25],
+            top:[0,10, 20,40,50,50,50,50,-50,-40,-20,-10],
+            left:[0,-0.5,-2.78,-10,0,0,0,200,200,-20,-7.4,-2.3],
             fontSize : [50,0,0,0,0,0,0,0,0,0,0,0]
         };
         this.transitionSpeed = [300, 150, 100];
@@ -18,9 +20,10 @@ export class DataBase{
         for(let i=0; i<this.MonSection.length; i++){
             
             this.MonSection[i].style.transition = 'all 0.3s linear 0s'; //deg각도대로 움직일 때의 속도
-            this.MonSection[i].style.transform = `rotate(${this.monDeg.deg[i]}deg) translate(${this.monDeg.moveX[i]}rem, ${this.monDeg.moveY[i]}rem)`;
-            
+            this.MonSection[i].style.transform = `rotate(${this.monDeg.deg[i]}deg)`;
             this.MonSection[i].style.fontSize = `${this.monDeg.fontSize[i]}px`;
+            this.MonSection[i].style.top = `${this.monDeg.top[i]}rem`;
+            this.MonSection[i].style.left = `${this.monDeg.left[i]}rem`;
         }        
     }
 }
