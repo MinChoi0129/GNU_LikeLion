b650c3015660a7beebdf4fae023c37ada04b1608
suyonnnn <134384063+suyonnnn@users.noreply.github.com>
2023-11-21 19:22:28 +0900

    활동기록상세페이지

--- static/js/activityDetail.js
+++ static/js/activityDetail.js
@@ -1,46 +1,66 @@
-//사진넘기는 기능
+//사진 넘기는 기능
 document.addEventListener("DOMContentLoaded", function () {
-    var imageList = [
-      "../static/image/hackathon.png",
-      "../static/image/OT.png",
-      "../static/image/hackathon.png",
-      "../static/image/OT.png",
-      "../static/image/hackathon2.png",
-      "../static/image/OT.png",
-    ];
-  
-    var currentIndex = 0;
-    var imageContainers = document.querySelectorAll(".etcimg img");
-    var sliderPages = document.querySelectorAll(".slider .page");
-  
-    function updateImageState() {
-      for (var i = 0; i < imageContainers.length; i++) {
-        var imageIndex = (currentIndex + i) % imageList.length;
+  var imageList = [
+    "../static/image/hackathon.png",
+    "../static/image/OT.png",
+    "../static/image/hackathon2.png",
+    "../static/image/lion.png",
+    // "../static/image/hackathon2.png",
+    // "../static/image/OT.png",
+  ];
+
+  // 중복된 항목 제거
+  imageList = Array.from(new Set(imageList));
+
+  var currentIndex = 0;
+  var imageContainers = document.querySelectorAll(".etcimg img");
+  var sliderPages = document.querySelectorAll(".slider .page");
+  var rightButton = document.querySelector(".rightarrow");
+  var leftButton = document.querySelector(".leftarrow");
+
+  function updateImageState() {
+    for (var i = 0; i < imageContainers.length; i++) {
+      var imageIndex = (currentIndex + i) % imageList.length;
+      if (i < imageList.length) {
         imageContainers[i].src = imageList[imageIndex];
+        imageContainers[i].style.display = "block";
+      } else {
+        imageContainers[i].style.display = "none";
       }
-  
-      sliderPages.forEach(function (page, index) {
-        if (index === currentIndex) {
-          page.classList.add("sliderActive");
-        } else {
-          page.classList.remove("sliderActive");
-        }
-      });
     }
-  
+
+    sliderPages.forEach(function (page, index) {
+      if (index === currentIndex) {
+        page.classList.add("sliderActive");
+      } else {
+        page.classList.remove("sliderActive");
+      }
+    });
+
+    // 이미지 리스트의 길이에 따라 화살표를 표시 또는 숨김
+    if (imageList.length <= 3) {
+      rightButton.style.display = "none";
+      leftButton.style.display = "none";
+    } else {
+      rightButton.style.display = "block";
+      leftButton.style.display = "block";
+    }
+  }
+
+  updateImageState();
+
+  rightButton.addEventListener("click", function () {
+    currentIndex = (currentIndex + imageList.length % 3) % imageList.length;
     updateImageState();
-  
-    var rightButton = document.querySelector(".rightarrow");
-    rightButton.addEventListener("click", function () {
-      currentIndex = (currentIndex + 3) % imageList.length;
-      updateImageState();
-    });
-  
-    var leftButton = document.querySelector(".leftarrow");
-    leftButton.addEventListener("click", function () {
-      currentIndex = (currentIndex - 3 + imageList.length) % imageList.length;
-      updateImageState();
-    });
   });
 
-//화살표 관련
+  leftButton.addEventListener("click", function () {
+    currentIndex = (currentIndex - imageList.length % 3 + imageList.length) % imageList.length;
+    updateImageState();
+  });
+});
+
+
+
+
+
