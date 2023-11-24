867ad1c482ad1ea710b7259ffb346ad7e8135147
rightnowjyk <rightnotjyk>
2023-11-25 01:07:38 +0900

    modify

--- static/js/project.js
+++ static/js/project.js
@@ -79,7 +79,62 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function changeYear(year) {
   // currentYear 엘리먼트에 year의 내용을 설정
-  document.getElementById('currentYear').innerText = year;
+  document.getElementById("currentYear").innerText = year;
 
   /* 기수마다 사진과 설명  추가*/
 }
+
+// Array to store image paths
+var imagePaths = [
+  "../static/image/ideathon1.png",
+  "../static/image/ideathon2.png",
+  "../static/image/ideathon3.png",
+  "../static/image/ideathon1.png",
+  "../static/image/ideathon2.png",
+  "../static/image/ideathon3.png",
+];
+
+// Array to store descriptions for each image
+var imageDescriptions = [
+  { account: "더 나은 건강, 더 행복한 삶", name: "RE : BORN" },
+  { account: "필요한 약을 클릭 한번으로", name: "PHAMPHAM" },
+  { account: "도대체 가능한 날이 언제야", name: "이때 어때" },
+  { account: "더 나은 건강, 더 행복한 삶", name: "RE : BORN" },
+  { account: "필요한 약을 클릭 한번으로", name: "PHAMPHAM" },
+  { account: "도대체 가능한 날이 언제야", name: "이때 어때" },
+];
+
+// Variable to track the current image index
+var currentIndex = 0;
+
+var staticURL = "/static/image/more.png"; // 정적 파일의 실제 경로로 수정
+
+// Function to change the description based on the selected image index
+function changeDescription(index) {
+  var descriptionElement = document.querySelector(".explanation .account");
+  var currentDescription = imageDescriptions[index];
+
+  descriptionElement.innerHTML = `
+      <div class="account">${currentDescription.account}
+          <div class="name">${currentDescription.name}</div>
+          <div class="view">VIEW MORE 
+                <img class="more" src="${staticURL}" />
+          </div>    
+      </div>`;
+}
+
+// Event listeners for left and right buttons to change image and description
+document.getElementById("leftBtn").addEventListener("click", function () {
+  // Decrease the current index (looping back to the last image if at the first image)
+  currentIndex = (currentIndex - 1 + imagePaths.length) % imagePaths.length;
+  changeDescription(currentIndex);
+});
+
+document.getElementById("rightBtn").addEventListener("click", function () {
+  // Increase the current index (looping back to the first image if at the last image)
+  currentIndex = (currentIndex + 1) % imagePaths.length;
+  changeDescription(currentIndex);
+});
+
+// Initial call to display the first image description
+changeDescription(currentIndex);
