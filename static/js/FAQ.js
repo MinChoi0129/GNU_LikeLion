3a599f599a90c6aa65c80c6fec9aade843ccb5c9
Paik JongHwa <qorwhdghk12@gmail.com>
2023-11-26 00:23:31 +0900

    진행사항 저장

--- static/js/FAQ.js
+++ static/js/FAQ.js
@@ -3,7 +3,7 @@ const question = [
   "세션은 어떻게 진행되나요?",
   "세션을 통해 뭘 얻을 수 있나요?",
 ];
-console.log("faq입니다")
+console.log("faq입니다");
 const answer = [
   "네, 매주 세션 하나씩 진행됩니다.",
   "세션은 2시간 동안 진행됩니다.",
@@ -11,52 +11,58 @@ const answer = [
 ];
 function show_faqs() {
   for (let i = 0; i < question.length; i++) {
-    html = `<div class="faq-content-title"><img src="{% static 'image/plus.png' %}" style=" height:2vh; text-align: center;"
-                class="plus"><img src="{% static 'image/minus.png' %}" style=" height:2vh; text-align: center;" class="minus">
+    if (i == 0) {
+      html = `<div class="faq-content-title">
               &nbsp;
               ${question[i]}
             </div>
             <div class="faq-content">${answer[i]}</div>`;
-    document.getElementsByClassName("faq-list")[0].innerHTML += html;
+      document.getElementById("faq-list").innerHTML += html;
+    } else {
+      html = `<hr><div class="faq-content-title">
+              &nbsp;
+              ${question[i]}
+            </div>
+            <div class="faq-content">${answer[i]}</div>`;
+      document.getElementById("faq-list").innerHTML += html;
+    }
   }
 }
-function show_faq(n) {
-  html = `<div class="faq-content-title"><img src="{% static 'image/plus.png' %}" style=" height:2vh; text-align: center;"
-                class="plus"><img src="{% static 'image/minus.png' %}" style=" height:2vh; text-align: center;" class="minus">
-              &nbsp;
-              ${question[n]}
-            </div>
-            <div class="faq-content">${answer[n]}</div>`;
-  document.getElementsByClassName("faq-list")[0].innerHTML += html;
-}
-show_faqs();
-
-const faqItems = document.getElementsByClassName("faq-content-title");
-const plus = document.getElementsByClassName("plus");
-const minus = document.getElementsByClassName("minus");
-for (let i = 0; i < minus.length; i++) {
-  minus[i].hidden = true;
-}
-for (let i = 0; i < faqItems.length; i++) {
-  faqItems[i].nextElementSibling.hidden = true;
+function show_faq(n, i) {
+  if (i == 0) {
+    html = `<div class="faq-content-title">
+            &nbsp;
+            ${question[n]}
+          </div>
+          <div class="faq-content">${answer[n]}</div>`;
+    document.getElementById("faq-list").innerHTML += html;
+  } else {
+    html = `<hr><div class="faq-content-title">
+                &nbsp;
+                ${question[n]}
+              </div>
+              <div class="faq-content">${answer[n]}</div>`;
+    document.getElementById("faq-list").innerHTML += html;
+  }
 }
 
-for (let i = 0; i < faqItems.length; i++) {
-  faqItems[i].addEventListener("click", function () {
-    if (faqItems[i].nextElementSibling.hidden === true) {
-      faqItems[i].nextElementSibling.hidden = false;
-      plus[i].hidden = true;
-      minus[i].hidden = false;
-    } else {
-      faqItems[i].nextElementSibling.hidden = true;
-      plus[i].hidden = false;
-      minus[i].hidden = true;
-    }
-  });
+function addfaqEvent() {
+  const faqItems = document.getElementsByClassName("faq-content-title");
+
+  for (let i = 0; i < faqItems.length; i++) {
+    faqItems[i].nextElementSibling.hidden = true;
+    faqItems[i].addEventListener("click", function () {
+      if (faqItems[i].nextElementSibling.hidden === true) {
+        faqItems[i].nextElementSibling.hidden = false;
+      } else {
+        faqItems[i].nextElementSibling.hidden = true;
+      }
+    });
+  }
 }
 
 function search() {
-  document.getElementsByClassName("faq-list")[0].innerHTML = "";
+  document.getElementById("faq-list").innerHTML = "";
   let input = document.getElementById("search").value;
   input = input.toLowerCase();
   search_result = new Set();
@@ -68,9 +74,13 @@ function search() {
       search_result.add(i);
     }
   }
-  search_result.forEach((index) => {
-    show_faq(index);
-  });
+  search_result = Array.from(search_result);
+  for (let i = 0; i < search_result.length; i++) {
+    show_faq(search_result[i], i);
+  }
+  addfaqEvent();
 }
 
+show_faqs();
+addfaqEvent();
 // 애니메이션 추가(내려오는 거, + - 등 추가적인 것들), 이미지Path수정, 검색 시 이벤트 리스너 추가(or 다른 방법), 검색 창 비울 시 전체 목록 보여주기
