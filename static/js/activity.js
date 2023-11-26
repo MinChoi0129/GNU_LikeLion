c4f0e2bf154739d55dff2c5570f69bb1b6790be4
psuno01 <p20035357@gmail.com>
2023-11-26 20:06:31 +0900

    1126

--- static/js/activity.js
+++ static/js/activity.js
@@ -1,16 +1,17 @@
+// 10th, 11th 버튼 클릭 이벤트
 document.addEventListener("DOMContentLoaded", function () {
 
-    var slideBarIcon = document.querySelector('.firstPageSlideBarIcon');
-    var firstPage = document.querySelector('.firstPage');
-    var secondPage = document.querySelector('.secondPage');
+    // 10th, 11th 버튼 클릭 이벤트
+    const yearButtons = document.querySelectorAll(".dep2Year");
 
-
-
-    slideBarIcon.addEventListener('click', function () {
-        firstPage.classList.toggle('hidden');
-        secondPage.classList.toggle('visible');
+    yearButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            selectedYear = button.textContent.replace(/\D+/g, ''); // 클릭한 버튼의 텍스트에서 숫자만 추출
+            currentIndex2 = 0; // 선택된 년도로 변경될 때 초기 인덱스 0으로 설정
+            currentYearElement.textContent = selectedYear + "th."; // 현재 년도 업데이트
+            updateImageState(); // 이미지와 설명 업데이트
+        });
     });
-
 
     const postList = document.querySelector(".post-list");
     const nextButton = document.getElementById("nextButton");
@@ -32,6 +33,4 @@ document.addEventListener("DOMContentLoaded", function () {
             shiftPosts();
         }, 50);
     });
-
-
 });
