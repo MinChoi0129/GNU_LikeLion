56792bffcb72f1b3ed1761f868104d5a223f499a
seajihey <okcoco03@naver.com>
2023-11-30 15:02:16 +0900

    ddd

--- static/js/restApiData.js
+++ static/js/restApiData.js
@@ -13,3 +13,15 @@ export async function getProjectDataById(projectId) {
         }
       }
 
+export async function getPostDataById(postid) {
+const apiUrl = `http://127.0.0.1:8000/api/posts/${postid}/?format=json`;
+
+try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data;
+} catch (error) {
+    console.error('Error fetching project data:', error);
+    return null;
+}
+}
